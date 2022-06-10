29d2a2027bb258278ccecef3298997519f9eab41
benacekp <benacekpetr@gmail.com>
2022-06-10 09:44:13 +0200

    timer collection control, timer card control

---
+++ KitchenTimeXamarin/KitchenTimeXamarin/Controls/TimerCard.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KitchenTimeXamarin.Controls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TimerCard : ContentView
+	{
+		public TimerCard()
+		{
+			InitializeComponent();
+		}
+	}
+}
